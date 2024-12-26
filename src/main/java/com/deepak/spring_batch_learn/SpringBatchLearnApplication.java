8dfe5fd9b1bf98d49723bc9357ad23d09ac53f55
Deepak Sriram <dsriram@griddynamics.com>
2024-12-26 11:53:46 +0530

    Initialized spring_batch_learn project

---
+++ src/main/java/com/deepak/spring_batch_learn/SpringBatchLearnApplication.java
@@ -0,0 +1,11 @@
+package com.deepak.spring_batch_learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchLearnApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchLearnApplication.class, args);
+	}
+}
