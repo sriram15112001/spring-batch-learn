450baf94b32b781fc298d9c8ab48195a508db9c0
Deepak Sriram <dsriram@griddynamics.com>
2024-12-26 14:40:45 +0530

    Added writer

--- src/main/java/com/deepak/spring_batch_learn/config/MyConfiguration.java
+++ src/main/java/com/deepak/spring_batch_learn/config/MyConfiguration.java
@@ -1,10 +1,13 @@
 package com.deepak.spring_batch_learn.config;
 
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class MyConfiguration {
@@ -16,6 +19,15 @@ public class MyConfiguration {
                 .lineMapper((line, lineNumber) -> {
                     return line;
                 })
+                .build();
+    }
+
+    @Bean
+    public FlatFileItemWriter<String> writer() {
+        return new FlatFileItemWriterBuilder<String>()
+                .resource(new FileSystemResource("src/main/resources/masked-data.csv"))
+                .name("csv-writer")
+                .lineAggregator(line -> line)
                 .build();
     }
 
